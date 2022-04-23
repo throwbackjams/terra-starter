108da34b47826114ddb374000c660d507698a073
throwbackjams <yli17@alumni.nd.edu>
2022-04-23 15:23:35 -0500

    setup frontend with wallet connect button

--- src/App.js
+++ src/App.js
@@ -1,23 +1,57 @@
 import './App.css';
+import { useWallet, WalletStatus } from "@terra-money/wallet-provider";
 
 function App() {
+  const { status, connect, disconnect, availableConnectTypes } = useWallet();
+
+  const renderConnectButton = () => {
+    if (status === WalletStatus.WALLET_NOT_CONNECTED) {
+      return (
+        <div className="connect-wallet-div">
+          <button
+            type="button"
+            key={`connect-EXTENSION`}
+            onClick={() => connect("EXTENSION")}
+            className="cta-button connect-wallet-button"
+          >
+            Connect Wallet
+          </button>
+        </div>
+      );
+    }
+    else if (status === WalletStatus.WALLET_CONNECTED) {
+      return(
+        <button
+          type="button"
+          onClick={() => disconnect()}
+          className="cta-button connect-wallet-button"
+        >
+          Disconnect
+        </button>
+      );
+    }
+  };
+
+  console.log("Wallet status is ", status);
+  console.log("available connection types: ", availableConnectTypes);
 
   return (
     <main className="App">
       <header>
         <div className="header-titles">
-          <h1>⚔ Goblin War ⚔</h1>
-          <p>Only you can save us from Goblin town</p>
+          <h1>🌕 Whack-a-Moon 🌕</h1>
+          <p>Get those LUNA Moons!</p>
         </div>
 
       </header>
 
       <div>
         <img
-          src="https://media.giphy.com/media/B19AYwNXoXtcs/giphy.gif"
-          alt="Goblin gif"
+          src="https://media.giphy.com/media/fr6EMszdu9vXxy9zkJ/giphy.gif"
+          alt="Terra Gif"
         />
       </div>
+      {renderConnectButton()}
     </main>
   );
 }
--- src/index.js
+++ src/index.js
@@ -5,33 +5,39 @@ import ReactDOM from 'react-dom';
 import { Route, Routes, BrowserRouter } from 'react-router-dom';
 import './index.css';
 
-const TWITTER_HANDLE = '_buildspace';
+import { getChainOptions, WalletProvider } from '@terra-money/wallet-provider';
+
+const TWITTER_HANDLE = 'hashequilibrium';
 const TWITTER_LINK = `https://twitter.com/${TWITTER_HANDLE}`;
 
-ReactDOM.render(
-  <React.StrictMode>
-      <div className="App-header">
-        <BrowserRouter>
-          <Routes>
-            <Route path="/" element={<App />} />
-          </Routes>
-        </BrowserRouter>
+getChainOptions().then((chainOptions) => {
+  console.log(chainOptions)
+  ReactDOM.render(
+    <React.StrictMode>
+      <WalletProvider {...chainOptions}>
+        <div className="App-header">
+          <BrowserRouter>
+            <Routes>
+              <Route path="/" element={<App />} />
+            </Routes>
+          </BrowserRouter>
 
-        <div className="footer-container">
-          <img
-            alt="Twitter Logo"
-            className="twitter-logo"
-            src="/twitter-logo.svg"
-          />
-          <a
-            className="footer-text"
-            href={TWITTER_LINK}
-            target="_blank"
-            rel="noreferrer"
-          >{`Made with @${TWITTER_HANDLE}`}</a>
+          <div className="footer-container">
+            <img
+              alt="Twitter Logo"
+              className="twitter-logo"
+              src="/twitter-logo.svg"
+            />
+            <a
+              className="footer-text"
+              href={TWITTER_LINK}
+              target="_blank"
+              rel="noreferrer"
+            >{`Made by @${TWITTER_HANDLE}`}</a>
+          </div>
         </div>
-      </div>
-
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+        </WalletProvider>
+    </React.StrictMode>,
+    document.getElementById('root')
+  );
+});
